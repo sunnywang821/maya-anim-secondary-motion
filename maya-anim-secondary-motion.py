222e69a93fcfa1b7a1982cf3bf42e308b4215097
sunnywang821 <sunnywang821@gmail.com>
2025-06-03 10:04:25 -0400

    Update maya-anim-secondary-motion.py

--- maya-anim-secondary-motion.py
+++ maya-anim-secondary-motion.py
@@ -11,12 +11,12 @@ Error handling:
 Functions:
 - getting world space of object at given frames
 - getting velocity
-- creating locator based off of given location
+- creating loc based off of given location
 
 Base logic:
 - Get position of selected object and its parent
-- Create locators at the object position and the parent's position 
-	The parent locator is going to have keys throughout so it matches parent location
+- Create locs at the object position and the parent's position 
+	The parent loc is going to have keys throughout so it matches parent location
 	every frame with the given frame range
 	
 """
@@ -57,11 +57,22 @@ if transformation is frozen
 """
 def get_world_space_at_frame(which_obj, frame):
 	cmds.currentTime(frame, edit=True)
-	world_pos = np.array(cmds.pointPosition(which_obj+".scalePivot", world=1))
+	world_pos = np.array(cmds.pointPosition(which_obj+".scalePivot", world=True))
 	# print(f"World position of {obj} at frame {frame}: {world_pos}")
 	return world_pos
 # remove "#" to test:
 # print(get_world_space_at_frame(parent_obj, 1))
+
+
+# Separate function for getting world position for locator because translate actualyl works
+# and pointPosition doesn't work
+def get_loc_world_space_at_frame(which_loc, frame):
+	cmds.currentTime(frame, edit=True)
+	world_pos = np.array(cmds.xform(which_loc, query=True, worldSpace=True, translation=True))
+	# print(f"World position of {obj} at frame {frame}: {world_pos}")
+	return world_pos
+# remove "#" to test:
+# print(get_loc_world_space_at_frame("locator1", 1))
 
 
 # Calculating velocity based on given values
@@ -70,47 +81,50 @@ def compute_velocity(position1, position2, time_delta):
 	return (position2 - position1) / time_delta
 
 
-# Create locator based on the given location
-def creat_locator_at_position(transform_values=[0, 0, 0], name="NameThis"):
-	locator = cmds.spaceLocator(name=name)
-	cmds.move(transform_values[0], transform_values[1], transform_values[2], locator, relative=True)
-	return locator
+# Create loc based on the given location
+def creat_loc_at_position(transform_values=[0, 0, 0], name="NameThis"):
+	loc = cmds.spaceLocator(name=name)
+	cmds.move(transform_values[0], transform_values[1], transform_values[2], loc, relative=True)
+	return loc
 # remove "#" to test:
-# creat_locator_at_position()
+# creat_loc_at_position()
 
 '''
-# need to delete the locators after using them
-if parent_locator: 
-	cmds.delete(parent_locator)
-if obj_locator:
-	cmds.delete(obj_locator)
+# need to delete the locs after using them
+if parent_loc: 
+	cmds.delete(parent_loc)
+if obj_loc:
+	cmds.delete(obj_loc)
 '''
 
-# create a locator at the at the parent object's position
+# create a loc at the at the parent object's position
 # and key it by following the parent obj according to the frame range
 pos_current_parent = get_world_space_at_frame(parent_obj, start_frame)
-parent_locator = creat_locator_at_position(pos_current_parent, "ParentLocator")
+parent_loc = creat_loc_at_position(pos_current_parent, "Parentloc")
 
 for frame in range(start_frame, end_frame + 1):
 	pos_current_parent = get_world_space_at_frame(parent_obj, frame)
-	cmds.move(pos_current_parent[0], pos_current_parent[1], pos_current_parent[2], parent_locator, worldSpace=True)
-	cmds.setKeyframe(parent_locator, attribute="translate", t=frame)
+	cmds.move(pos_current_parent[0], pos_current_parent[1], pos_current_parent[2], parent_loc, worldSpace=True)
+	cmds.setKeyframe(parent_loc, attribute="translate", t=frame)
 
 
-# create a locator at the starting frame of the object's position and key it
+# create a loc at the starting frame of the object's position and key it
 pos_current_obj = get_world_space_at_frame(obj, start_frame)
-obj_locator = creat_locator_at_position(pos1_obj, "ObjLocator")
-cmds.setKeyframe(obj_locator, attribute="translate", t=start_frame)
+obj_loc = creat_loc_at_position(pos1_obj, "ObjLoc")
+cmds.setKeyframe(obj_loc, attribute="translate", t=start_frame)
 
 
 #############################################################################################
-# move the object to where the locator(child) is for the frame range
+# move the object to where the loc(child) is for the frame range
 # this doesn't move still idk
 for frame in range(start_frame, end_frame + 1):
     cmds.currentTime(frame)
     
+    pos_current_obj_loc = get_loc_world_space_at_frame(obj_loc, frame)
+    pos_previous_obj_loc = get_loc_world_space_at_frame(obj_loc, frame)
+    print(pos_current_obj_loc)
     # Verlet Integration
-    
+    # pos_next_obj_loc = pos_current_obj_loc + (pos_current_obj_loc - pos_previous_obj_loc) + 
 
 
 
